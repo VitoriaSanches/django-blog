e34d0668d04295830a8760104c2b7e6898d0c578
Vitoria Sanches <email od vitoriaferreirasanches@gmail.com>
2023-08-09 10:09:24 -0300

    Aula 09-08

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'index.html', {'titulo': 'Últimos Artigos'})
+
 def ola(request):
     # return HttpResponse('Olá Django')
-    return HttpResponse('Olá Django')
+    return render(request, 'home.html')
