cdf44fb3d88d8b7065b37a61a92cfa533888b7c5
Vitoria Sanches <email od vitoriaferreirasanches@gmail.com>
2023-08-23 10:04:28 -0300

    estudo 5 - django 4.2

--- webapp/settings.py
+++ webapp/settings.py
@@ -78,11 +78,14 @@ WSGI_APPLICATION = 'webapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'django_blog_db',
+    'USER': 'root',
+    'PASSWORD': '',
+    'HOST': 'localhost',
+    'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
