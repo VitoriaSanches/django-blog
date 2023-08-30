e37af02ce5b6c3dada8cfe446aeaeab00b02efd1
Vitoria Sanches <email od vitoriaferreirasanches@gmail.com>
2023-08-30 10:52:28 -0300

    aula 30-08

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,26 @@ from django.urls import path
 from blog.views import index, ola
 
 urlpatterns = [
-    path('index/', index, name="index"),
-    path('ola/', ola, name="ola")
+    path("index/", index, name="index"),
+    path("ola/", ola, name="ola"),
+    path("posts/all", ola, name="posts_list"),
+]
+
+from blog.views import index, ola, post_show  # Nesta linha importamos as views
+
+urlpatterns = [
+    path("index/", index, name="index"),
+    path("ola/", ola, name="ola"),
+    path("posts/all", ola, name="posts_list"),
+    path("post/<int:post_id>", post_show, name="exibe_post"),
+]
+
+from blog.views import index, ola, post_show, PostDetailView
+
+urlpatterns = [
+    path("index/", index, name="index"),
+    path("ola/", ola, name="ola"),
+    path("posts/all", ola, name="posts_list"),  # a view responsável é ola()
+    path("post/<int:post_id>", post_show, name="exibe_post"),
+    path("post/<int:pk>/show", PostDetailView.as_view(), name="post_detail"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,18 +1,42 @@
+from django.views.generic.detail import DetailView
+
+from blog.models import Post
+
 from django.shortcuts import render
 
 from django.http import HttpResponse
 
-from blog.models import Post 
+from blog.models import Post
+
 
 def index(request):
-    return render(request, 'index.html', {'titulo': 'Últimos Artigos'})
+    return render(request, "index.html", {"titulo": "Últimos Artigos"})
+
 
 def ola(request):
     # return HttpResponse('Olá Django')
-   return render(request, 'home.html')
+    return render(request, "home.html")
 
-def ola(request): # Modificar
+
+def ola(request):  # Modificar
     # return HttpResponse('Olá django')
-    posts = Post.objects.all() # recupera todos os posts do banco de dados
-    context = {'posts_list': posts } # cria um dicionário com os dado
-    return render(request, 'posts.html', context) # renderiza o template e passa o contexto
+    posts = Post.objects.all()  # recupera todos os posts do banco de dados
+    context = {"posts_list": posts}  # cria um dicionário com os dado
+    return render(
+        request, "posts.html", context
+    )  # renderiza o template e passa o contexto
+
+
+from django.shortcuts import render, get_object_or_404
+from blog.models import Post
+
+
+def post_show(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    return render(request, "post/detail.html", {"post": post})
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "post/detail.html"
+    context_object_name = "post"
